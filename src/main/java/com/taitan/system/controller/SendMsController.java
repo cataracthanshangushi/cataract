f223beecd66c07055cd1d17507f6b1705513a301
zoubo <849381081@qq.com>
2023-09-19 15:05:36 +0800

    更新短信功能

---
+++ src/main/java/com/taitan/system/controller/SendMsController.java
@@ -0,0 +1,29 @@
+package com.taitan.system.controller;
+
+import com.taitan.system.common.result.Result;
+import com.taitan.system.service.AliSmsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@Tag(name = "短信操作接口")
+@RestController
+@RequestMapping("/api/v1/message")
+@RequiredArgsConstructor
+public class SendMsController {
+
+    private final AliSmsService aliSmsService;
+
+    @GetMapping("/sendcode")
+    @Operation(summary = "发短信")
+    public Result uploadFile(
+            @Parameter(description = "手机号") @RequestParam String phone,
+            @Parameter(description = "验证码") @RequestParam String code
+    ) {
+        return Result.judge(aliSmsService.sendCode(phone,code));
+    }
+
+}
